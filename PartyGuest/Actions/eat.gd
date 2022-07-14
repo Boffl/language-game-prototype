2ceb6c48af6eff44e7e3cf35aca232326fe88a2b
jeankesselring <jean.kesselring@gmail.com>
2022-07-14 14:39:24 +0200

    eat action improved

--- PartyGuest/Actions/eat.gd
+++ PartyGuest/Actions/eat.gd
@@ -2,13 +2,19 @@ class_name eat
  
 extends Node2D
 
+var action_name = "eat"
+
 func prerequisite(guest):
-	print("thirst %s" %guest.thirst)
+	return guest.have_food > 0
 	
 	
-func heuristic():
-	pass
+func heuristic(guest):
+	return guest.hunger
 	
 
-func effect():
-	pass
+func effect(guest):
+	print("hunger before eating %s" %guest.hunger)
+	guest.hunger -= 0.5
+	guest.intoxication -= 0.05
+	guest.have_food -= 1
+	print("hunger after eating %s" %guest.hunger)
