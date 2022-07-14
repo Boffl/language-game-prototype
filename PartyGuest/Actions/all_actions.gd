c02624f023e4a8073b9fb0c8d52355f83d1ea345
jeankesselring <jean.kesselring@gmail.com>
2022-07-14 14:38:13 +0200

    Best action selection improved

--- PartyGuest/Actions/all_actions.gd
+++ PartyGuest/Actions/all_actions.gd
@@ -7,15 +7,17 @@ var dance = load("res://PartyGuest/Actions/dance.gd").new()
 var leave = load("res://PartyGuest/Actions/leave.gd").new()
 
 
-var actions = [drink_water]#, drink_alc, eat, dance, leave]
+var actions = [drink_water, eat]#, drink_alc, eat, dance, leave]
 
 func best_action(guest, depth=1):#iterative deepening must be implemented
 	var best_r = -50
 	var best_a = false
 	for action in actions:
+		print("%s %s" %[action.action_name, action.heuristic(guest)])
 		if action.prerequisite(guest):
 			if action.heuristic(guest) > best_r:
 				best_r = action.heuristic(guest)
 				best_a = action
-	print("best action %s" %best_a.action_name)
+	print("best action for %s is %s" %[guest.bot_name, best_a.action_name])
+	best_a.effect(guest)
 	return best_a
