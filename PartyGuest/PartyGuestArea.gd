f9f36abaca1f5835f46863b596f2ca26c38b2625
nikob <nikolaj.bauer@uzh.ch>
2022-07-18 23:56:02 +0200

    now removed the functions, didn't save before...

--- PartyGuest/PartyGuestArea.gd
+++ PartyGuest/PartyGuestArea.gd
@@ -1,12 +1,4 @@
 extends Area2D
 
-# onready var chatBox = get_node("CanvasLayer/ChatBox")
-
-func _on_PartyGuestArea_body_entered(body):
-	pass
-
-
-func _on_PartyGuestArea_body_exited(body):
-	pass
 
 
