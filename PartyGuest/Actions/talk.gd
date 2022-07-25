117115c634d4e1a1cb9b56048e6d3c850bc6b716
jeankesselring <jean.kesselring@gmail.com>
2022-07-25 15:48:02 +0200

    added wait time to actions, the waiting is done in the party script

--- PartyGuest/Actions/talk.gd
+++ PartyGuest/Actions/talk.gd
@@ -2,13 +2,18 @@ class_name talk
  
 extends Node2D
 
+var action_name = "talk"
+
 func prerequisite(guest):
-	print("thirst %s" %guest.thirst)
+	return guest.have_food > 0
 	
 	
-func heuristic():
-	pass
+func heuristic(guest, other_guest):
+	return guest.sociability + guest.like_other_agent[other_guest]
 	
 
-func effect():
-	pass
+func effect(guest):
+	#print("%s boredom before talking:  %s" %[guest.bot_name, guest.bored])
+	guest.bored -= 0.5
+
+	#print("%s boredom after talking:  %s" %[guest.bot_name, guest.bored])
