fae4f05515587a093c8a981ddbb4f1aaf710da48
jeankesselring <jean.kesselring@gmail.com>
2022-07-25 15:50:50 +0200

    breadth-first works for multiple iterations, but it is really slow

--- PartyGuest/Actions/all_actions.gd
+++ PartyGuest/Actions/all_actions.gd
@@ -9,27 +9,82 @@ var vomit = load("res://PartyGuest/Actions/vomit.gd").new()
 var talk = load("res://PartyGuest/Actions/talk.gd").new()
 
 
-var actions = [drink_water, eat, drink_alc, vomit, leave, dance]#, drink_alc, eat, dance, leave]
+var actions = [drink_water, drink_alc, eat, dance, leave, vomit]#, drink_alc, eat, dance, leave]
 var best_r 
 var best_a 
-var action_heuristic
+var reward
+var new_guest 
+var guest_dict
+var action_dict
 	
+var str_action_dict
+
+
 func best_action(guest, depth=1):#iterative deepening must be implemented
+	str_action_dict = {"drink water": drink_water, "drink alcohol": drink_alc, "eat": eat, "dance": dance, "leave": leave, "vomit": vomit}
+	
+	best_r = -50
+	best_a = false
+	guest_dict = {"":[0, guest]}
+	
+	for i in depth:
+		action_dict = {}
+		for key in guest_dict:
+			print(key)
+			for action in actions:
+				guest = guest_dict[key][1]
+				if action.prerequisite(guest):
+					reward = action.heuristic(guest) * ((10 - guest.past_actions.slice(0,10).count(best_a)) / 10) #diminishing return, the more a guest performs an action the less fun it is
+					new_guest = guest.duplicate()
+					new_guest.transfer_attributes(guest)
+					print(guest.thirst, "\t", new_guest.thirst)
+					action.effect(new_guest)
+					print(guest.thirst, "\t", new_guest.thirst)
+					action_dict[key + action.action_name + ","] = [reward, new_guest]
+		guest_dict = action_dict
+	print(guest_dict)
+	for key in guest_dict:
+		if guest_dict[key][0] > best_r:
+			best_a = key.split(",")[0]
+			best_r = guest_dict[key][0]
+	return str_action_dict[best_a]
+		#add rewards
+		#copy guest
+		#effect
+
+	
+	
+func talk_to_all(guest):
+	for other_guest in guest.like_other_agent:
+		if talk.heuristic(guest, other_guest) > best_r:
+			best_r = talk.heuristic(guest, other_guest)
+			best_a = talk
+			
+func sim_act(guest):
+	for action in actions:
+		#print("%s %s" %[action.action_name, action.heuristic(guest)])
+		if action.prerequisite(guest):
+			reward = action.heuristic(guest) * ((10 - guest.past_actions.slice(0,10).count(best_a)) / 10) #diminishing return, the more a guest performs an action the less fun it is
+			#print(stepify(action_heuristic,0.01),"\t", action.action_name)
+			if reward > best_r:
+				best_r = reward
+				best_a = action 
+
+func best_act(guest):
 	best_r = -50
 	best_a = false
 	var best_talk
 	for action in actions:
 		#print("%s %s" %[action.action_name, action.heuristic(guest)])
 		if action.prerequisite(guest):
-			action_heuristic = action.heuristic(guest) * ((10 - guest.past_actions.slice(0,10).count(best_a)) / 10) #diminishing return, the more a guest performs an action the less fun it is
+			reward = action.heuristic(guest) * ((10 - guest.past_actions.slice(0,10).count(best_a)) / 10) #diminishing return, the more a guest performs an action the less fun it is
 			#print(stepify(action_heuristic,0.01),"\t", action.action_name)
-			if action_heuristic > best_r:
-				best_r = action_heuristic
+			if reward > best_r:
+				best_r = reward
 				best_a = action 
-	#talk_to_all(guest)
-
-	#print("best action for %s is %s" %[guest.bot_name, best_a.action_name])
-	best_a.effect(guest)
+	new_guest = guest.duplicate()
+	#best_a.effect(guest_1)
+	print(new_guest.guest_name, guest.guest_name)
 	guest.past_actions.append(best_a)
 	#print(best_a.action_name)
 	var copy_guest = guest
@@ -37,11 +92,3 @@ func best_action(guest, depth=1):#iterative deepening must be implemented
 	#print(copy_guest.guest_name, guest.guest_name)
 	print(guest.guest_name, "\t", best_a.action_name)
 	return best_a
-	
-func talk_to_all(guest):
-	for other_guest in guest.like_other_agent:
-		if talk.heuristic(guest, other_guest) > best_r:
-			best_r = talk.heuristic(guest, other_guest)
-			best_a = talk
-
-
